a91912a70cbaf4bc68591155dc50977e64c2bd34
Dominik Tworek <dtworek94@gmail.com>
2023-11-22 04:16:46 +0000

    created hero section unresponsive prototype

--- frontend/electron-ecommerce-react/src/ui/home/Home.tsx
+++ frontend/electron-ecommerce-react/src/ui/home/Home.tsx
@@ -1,9 +1,33 @@
+import headphones from '../../assets/images/headphones_white.png';
+import {Link} from "react-router-dom";
+
 export const Home = () => {
 
 
     return (
-        <section>
-            Welcome in home page!
+        <section className={"px-responsive-electron w-full max-w-[1440px]"}>
+
+            <div className={"mt-[16px] h-[352px] rounded-3xl bg-electron-primary-dark-blue overflow-hidden relative"}>
+                <div
+                    className={"absolute w-[210px] h-[145px] blur-[100px] -rotate-[135deg] bg-gradient-to-t from-electron-gradient-light-blue to-electron-gradient-eye-blue"}/>
+                <div
+                    className={"absolute -right-0 w-[445px] h-[308px] blur-[100px] -rotate-[135deg] bg-gradient-to-t from-electron-gradient-light-blue to-electron-gradient-eye-blue"}/>
+                <img className={"absolute w-[552px] -top-[80px] right-[29px] "} src={headphones}
+                     alt={"transparent headphones image"}/>
+
+                <div className={"absolute top-[59px] left-[93px] text-[28px] text-electron-primary-white flex flex-col gap-7"}>
+                    <p className={"flex flex-col"}>
+                        <span>Journey Into Sound</span>
+                        <span className={"text-[62px]"}>Airpods Max</span>
+                    </p>
+                    <Link to={'/'}>
+                        <span className={"text-[20px] bg-electron-button-bg-blue py-4 px-8 rounded-[24px]"}>25% OFF</span>
+                    </Link>
+                </div>
+
+            </div>
+
+
         </section>
     )
 }
--- frontend/electron-ecommerce-react/tailwind.config.js
+++ frontend/electron-ecommerce-react/tailwind.config.js
@@ -1,19 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
-  theme: {
-    fontFamily: {
-      'inter': ['Inter', 'sans-serif']
+    content: ["./src/**/*.{js,jsx,ts,tsx}"],
+    theme: {
+        fontFamily: {
+            'inter': ['Inter', 'sans-serif']
+        },
+        colors: {
+            'electron-primary-dark-blue': '#0E1120',
+            'electron-primary-white': '#FFFFFF',
+            'electron-primary-grey': '#afafaf',
+            'electron-input-bg': '#262836',
+            'electron-input-border': 'rgba(240, 240, 240, 0.05)',
+            'electron-gradient-light-blue': '#4AA8FF',
+            'electron-gradient-eye-blue': '#00F0FF',
+            'electron-button-bg-blue': '#2287FD',
+        },
+        extend: {},
     },
-    colors: {
-      'electron-primary-dark-blue': '#0E1120',
-      'electron-primary-white': '#FFFFFF',
-      'electron-primary-grey' : '#afafaf',
-      'electron-input-bg': '#262836',
-      'electron-input-border': 'rgba(240, 240, 240, 0.05)',
-    },
-    extend: {},
-  },
-  plugins: [],
+    plugins: [],
 }
 
