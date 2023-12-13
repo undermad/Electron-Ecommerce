d79949395aa3dbf2f4f40a4fc4a5976ecceb1e97
Dominik Tworek <dtworek94@gmail.com>
2023-12-13 03:43:12 +0000

    fixed bug that hide expanded list of categories, give some responsivity to hero section and created not yet responsive login screen

--- frontend/electron-ecommerce-react/src/ui/nav/AccountLink.tsx
+++ frontend/electron-ecommerce-react/src/ui/nav/AccountLink.tsx
@@ -1,20 +1,22 @@
 import userSVG from "../../assets/icons/user.svg";
 import {useViewport} from "../custom_hooks/CustomHooks.ts";
 import {Breakpoints} from "../custom_hooks/Breakpoints.ts";
+import {Link} from "react-router-dom";
 
 export const Account = () => {
     const width = useViewport();
 
 
-
     return (
+        <Link to={"/account"}>
             <div className={"flex gap-[8px] items-center"}>
                 <img src={userSVG} className={"w-[20px] h-[20px]"} alt={'user svg icon'}/>
 
                 {width >= Breakpoints.LARGE ?
-                <span>Login</span>
-                : ''}
+                    <span>Login</span>
+                    : ''}
 
             </div>
+        </Link>
     )
 }
