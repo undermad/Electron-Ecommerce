d4a4390db62c1f2a3b2565a9eaa4591568c44bbb
Dominik Tworek <dtworek94@gmail.com>
2024-02-13 01:19:14 +0000

    Fixed links in footer

--- frontend/electron-ecommerce-react/src/ui/footer/Footer.tsx
+++ frontend/electron-ecommerce-react/src/ui/footer/Footer.tsx
@@ -2,7 +2,7 @@ import {Container} from "../reusable/Container.tsx";
 import {ElectronLogoSvg} from "../../assets/images/ElectronLogoSvg.tsx";
 import {ParagraphSmall} from "../reusable/ParagraphSmall.tsx";
 import {FooterColumn} from "./FooterColumn.tsx";
-import {SEARCH_ROUTE} from "../../constants/Routes.ts";
+import {HOME_ROUTE, SEARCH_ROUTE} from "../../constants/Routes.ts";
 import {FaLinkedin} from "react-icons/fa";
 import {FaGithub} from "react-icons/fa";
 import {FaXTwitter} from "react-icons/fa6";
@@ -10,11 +10,30 @@ import {FaXTwitter} from "react-icons/fa6";
 
 export const Footer = () => {
 
-    const items: Map<string, string> = new Map();
-    items.set('Graphic cards', SEARCH_ROUTE + "/gpu")
-    items.set('Memory', SEARCH_ROUTE + "/memory")
-    items.set('Monitors', SEARCH_ROUTE + "/monitors")
-    items.set('Headphones', SEARCH_ROUTE + "/headphones")
+    const productsColumn: Map<string, string> = new Map();
+    productsColumn.set('Graphic cards', SEARCH_ROUTE + "/gpu")
+    productsColumn.set('Memory', SEARCH_ROUTE + "/memory")
+    productsColumn.set('Monitors', SEARCH_ROUTE + "/monitors")
+    productsColumn.set('Headphones', SEARCH_ROUTE + "/headphones")
+
+    const companyLinks: Map<string, string> = new Map();
+    companyLinks.set('About', HOME_ROUTE);
+    companyLinks.set('Careers', HOME_ROUTE);
+    companyLinks.set('News', HOME_ROUTE);
+    companyLinks.set('Contact', HOME_ROUTE);
+
+    const socialLinks: Map<string, string> = new Map();
+    socialLinks.set('LinkedIn', 'https://www.linkedin.com/in/dominik-tworek-850b4b25a')
+    socialLinks.set('Github', 'https://www.github.com/undermad')
+    socialLinks.set('Twitter', 'https://twitter.com/TworekDominik')
+
+    const terms: Map<string, string> = new Map();
+    terms.set('Terms', HOME_ROUTE);
+    terms.set('Privacy', HOME_ROUTE);
+    terms.set('Cookies', HOME_ROUTE);
+    terms.set('Licenses', HOME_ROUTE);
+
+
 
 
     return (
@@ -27,10 +46,10 @@ export const Footer = () => {
                             discount</ParagraphSmall>
                     </figure>
                     <figure className={"w-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-[32px] justify-between"}>
-                        <FooterColumn name={"Product"} links={items}/>
-                        <FooterColumn name={"Product"} links={items}/>
-                        <FooterColumn name={"Product"} links={items}/>
-                        <FooterColumn name={"Product"} links={items}/>
+                        <FooterColumn name={"Product"} links={productsColumn} applicationLinks={true}/>
+                        <FooterColumn name={"Company"} links={companyLinks} applicationLinks={true}/>
+                        <FooterColumn name={"Social"} links={socialLinks} applicationLinks={false}/>
+                        <FooterColumn name={"Company"} links={terms} applicationLinks={true}/>
                     </figure>
                 </article>
                 <article className="flex flex-col md:flex-row gap-[12px]">
--- frontend/electron-ecommerce-react/src/ui/footer/FooterColumn.tsx
+++ frontend/electron-ecommerce-react/src/ui/footer/FooterColumn.tsx
@@ -3,9 +3,10 @@ import {Link} from "react-router-dom";
 type FooterColumnProps = {
     name: string,
     links: Map<string, string>,
+    applicationLinks: boolean
 }
 
-export const FooterColumn = ({name, links}: FooterColumnProps) => {
+export const FooterColumn = ({name, links, applicationLinks}: FooterColumnProps) => {
 
 
     return (
@@ -13,9 +14,14 @@ export const FooterColumn = ({name, links}: FooterColumnProps) => {
             <span className={"text-[14px] text-electron-placeholder-grey font-[600]"}>{name}</span>
             <ul className="flex flex-col gap-[12px]">
                 {Array.from(links).map(([key, value]) => (
-                    <Link to={value}>
-                        <li className="text-[16px] font-[600] text-electron-other-page">{key}</li>
-                    </Link>
+                    applicationLinks ?
+                        <Link to={value}>
+                            <li className="text-[16px] font-[600] text-electron-other-page">{key}</li>
+                        </Link>
+                        :
+                        <a href={value}>
+                            <li className="text-[16px] font-[600] text-electron-other-page">{key}</li>
+                        </a>
                 ))}
 
             </ul>
